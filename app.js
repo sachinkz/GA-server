42ce6fea40d76857e4396afe41edd03387833494
Sachin K <sachuiritty@gmail.com>
2024-03-04 21:48:48 +0530

    cors edited

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ const authRoutes = require('./routes/authRoutes')
 const app = express();
 
 app.use(cors({
-  // origin: 'https://www.grabarts.online',
-  origin:"http://localhost:3000",
+  origin: 'https://www.grabarts.online',
   methods:"GET,POST,PUT,PATCH,DELETE,OPTIONS",
 }))
 
